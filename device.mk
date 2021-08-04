bf27914477defad9b148cc559a6835d83fd6f138
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-04 17:22:14 +0530

    r5x: Override baseband version from modem firmware
     * realme (and oppo) snapdragon devices have a generic
       baseband version of "Q_V1_P14" so instead of that,
       fetch the real baseband version from modem firmware
       and override it

--- device.mk
+++ device.mk
@@ -263,6 +263,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.set_baseband.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc \
